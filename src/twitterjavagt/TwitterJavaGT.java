29fc14c18036861169080552a1c60c4ea42bd629
lvaqueiroperez <lvaqueiroperez@nautilus27.danielcastelao.org>
2019-04-09 10:03:12 +0200

    "Inicio con Twitter4J USANDO EL FICHERO PROPERTIES PARA TOKENS"

--- src/twitterjavagt/TwitterJavaGT.java
+++ src/twitterjavagt/TwitterJavaGT.java
@@ -31,37 +31,34 @@ public class TwitterJavaGT {
         //DEL "OAuth credentials" QUE HAY QUE HACER PRIMERO
         //LA HAREMOS A TRAVÉS DE UN FICHERO EN EL CLASSPATH
         //Y A TRAVÉS DE UN CÓDIGO (HACEN LO MISMO)
-        //EN ESTA RAMA USAREMOS EL CÓDIGO DE "ConfigurationBuilder":
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("hbtRjHYztajam6Tx9siRel0RW")
-                .setOAuthConsumerSecret("oe69cb28pOrfjWDUBPTwz9dMXXWJt9gvgcYiwpp3rE8r2RoHYD")
-                .setOAuthAccessToken("1108644141657935872-fl73frb9YLNjf9zL1zS3ftmxNNoumM")
-                .setOAuthAccessTokenSecret("jjdH9r6Xf2sZfI8pZP6dTR1OQ2HTGQfdAx5kWCZWYDDQJ");
+        //EN ESTA RAMA USAREMOS EL FICHERO "twitter4j.properties":
         ////////////////////////////////////////////////////////////////////////
         // The factory instance is re-useable and thread safe. !!!!
         //No vamos a poder tener más de un objeto twitter: se usa un
         //factory para crear un objeto acceder con él indirectamente a una clase que creará
         //y nos devolverá un objeto singleton (que hemos llamado "twitter" y es de
         //tipo twitter)
+        //SIGUE HACIENDO FALTA PARTE DEL BUILDER !!!!!!
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
 
 //// POST TWEET
-//        //creamos una variable String con el tweet que queremos publicar
-//        String latestStatus = "Hola qué tal, mu buena tarde a todo";
-//        //creamos variable tipo "Status" y le asociamos nuestro tweet de la siguiente manera:
-//        Status status = twitter.updateStatus(latestStatus);
-//        //mostramos por pantalla un mensaje en caso de que el tweet se haya publicado
-//        //correctamente (si no saldrá una excepción)
-//        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        //creamos una variable String con el tweet que queremos publicar
+        String latestStatus = "Hola qudrfddf";
+        //creamos variable tipo "Status" y le asociamos nuestro tweet de la siguiente manera:
+        Status status = twitter.updateStatus(latestStatus);
+        //mostramos por pantalla un mensaje en caso de que el tweet se haya publicado
+        //correctamente (si no saldrá una excepción)
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //GET USER'S TIMELINE
-        //UNA VEZ CREADO NUESTRO SINGLETON "Twitter", CREAMOS UN ARRAYLIST
-        //DONDE SE ALMACENARÁN LOS DATOS DE NUESTRA TIMELINE USANDO NUESTRO SINGLETON
+//        UNA VEZ CREADO NUESTRO SINGLETON "Twitter", CREAMOS UN ARRAYLIST
+//        DONDE SE ALMACENARÁN LOS DATOS DE NUESTRA TIMELINE USANDO NUESTRO SINGLETON
 //        List<Status> statuses = twitter.getHomeTimeline();
-//        //MOSTRAMOS LA TIMELINE RECORRIENDO EL ARRAYLIST COMO SIEMRPE Y ACCEDIENDO
-//        //A SUS DATOS, TODO CON UN FOREACH(SE PODRÍA RECORRER DE OTRAS MANERAS)
+//        MOSTRAMOS LA TIMELINE RECORRIENDO EL ARRAYLIST COMO SIEMRPE Y ACCEDIENDO
+//        A SUS DATOS, TODO CON UN FOREACH(SE PODRÍA RECORRER DE OTRAS MANERAS)
 //        System.out.println("Showing home timeline.");
 //        for (Status status : statuses) {
 //            System.out.println(status.getUser().getName() + ":"
@@ -71,7 +68,7 @@ public class TwitterJavaGT {
 //SEARCH FOR TWEETS: (ANY)
 //        //CREAMOS UNA VARIABLE "Query" A LA QUE ASOCIAREMOS UN OBJETO "Query"
 //        //CON EL STRING QUE QUEREMOS BUSCAR EN TWITTER
-//        Query query = new Query("Mi primer tweet con twitter4j");
+//        Query query = new Query("murciélago");
 //        //CREAMOS UNA VARIABLE "QueryResult" A LA QUE ASOCIAREMOS LOS RESULTÁDOS DE LA BÚSQUEDA:
 //        QueryResult result = twitter.search(query);
 //        //RECORREMOS LOS RESULTADOS DE LA VARIABLE "QueryResult" Y LOS ENSEÑAMOS COMO SI 
